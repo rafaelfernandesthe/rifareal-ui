fd56d15d5f5eea5a024e63cbd08e025e7f6bd8f6
Rafael Fernandes da Silva <rafa.elfernandes@hotmail.com>
2021-03-27 11:01:30 -0300

    feat: totalizadores por status

--- src/app/pages/rifa-detalhe/rifa-detalhe.component.ts
+++ src/app/pages/rifa-detalhe/rifa-detalhe.component.ts
@@ -66,9 +66,9 @@ export class RifaDetalheComponent extends PagesBaseComponent implements OnInit, 
 
         this.todosNumeros = this.todosNumeros.concat(this.rifa.numeros);
 
-        this.qtdDisponivel = this.todosNumeros.filter(n => n.status = 0).length;
-        this.qtdReservado = this.todosNumeros.filter(n => n.status = 1).length;
-        this.qtdPago = this.todosNumeros.filter(n => n.status = 2).length;
+        this.qtdDisponivel = this.todosNumeros.filter(n => n.status === 0).length;
+        this.qtdReservado = this.todosNumeros.filter(n => n.status === 1).length;
+        this.qtdPago = this.todosNumeros.filter(n => n.status === 2).length;
 
       });
 
